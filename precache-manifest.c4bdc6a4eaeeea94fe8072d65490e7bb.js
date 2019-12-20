ab6ae3ec3e2de91a2a6c99702b6a6019de4d408a
sn3474 <sn3474@att.com>
2019-12-20 17:42:03 -0500

    Updates

---
+++ precache-manifest.c4bdc6a4eaeeea94fe8072d65490e7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "007513a847f9e0fdaea8df72ae0e70bf",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "10e4f37733e94e462ba2",
+    "url": "/ContactManager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "c60129f0e50855564a58",
+    "url": "/ContactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "10e4f37733e94e462ba2",
+    "url": "/ContactManager/static/js/2.f5faf258.chunk.js"
+  },
+  {
+    "revision": "507033bb2273480b0ab9fb9e034f8f98",
+    "url": "/ContactManager/static/js/2.f5faf258.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c60129f0e50855564a58",
+    "url": "/ContactManager/static/js/main.1f86b977.chunk.js"
+  },
+  {
+    "revision": "970d37323c8c8ba129e5",
+    "url": "/ContactManager/static/js/runtime-main.fa4f050f.js"
+  }
+]);
